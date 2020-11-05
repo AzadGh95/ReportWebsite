ad89bade5c972ab44eab9f2eab73a6921a53c3a1
a.ghassemi92@gmail.com <a.ghassemi92@gmail.com>
2020-11-05 14:01:24 +0330

    users-L1

---
+++ ReportWebsite/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using ReportWebsite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReportWebsite.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Users()
+        {
+           
+            return View();
+
+        }
+    }
+}
